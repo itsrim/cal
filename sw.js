01b00b60a0ecefe35bd851d47321927472f2fafb
rim <toto31@yopmail.com>
2025-09-26 20:11:58 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1212,6 +1212,6 @@ function te(a) {
 function se(a, e) {
   te(a), ee(e);
 }
-se([{"revision":null,"url":"assets/index-2rY6ymVV.js"},{"revision":null,"url":"assets/index-UZxPTRyy.css"},{"revision":null,"url":"assets/workbox-window.prod.es5-B9K5rw8f.js"},{"revision":"d12ca56710e97bc90e741f0d8767a32c","url":"index.html"},{"revision":"f8f740894087805bed4200a1a656b912","url":"manifest.webmanifest"}]);
+se([{"revision":null,"url":"assets/index-BcqrVm1F.js"},{"revision":null,"url":"assets/index-UZxPTRyy.css"},{"revision":null,"url":"assets/workbox-window.prod.es5-B9K5rw8f.js"},{"revision":"e839181124a44de420f60d807156c39b","url":"index.html"},{"revision":"f8f740894087805bed4200a1a656b912","url":"manifest.webmanifest"}]);
 self.addEventListener("fetch", (a) => {
 });
